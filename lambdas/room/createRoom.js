c31d62689f4817357521b6397ed1c9c80510dbcf
Bjorler <gth86@hotmail.com>
2022-05-11 14:09:08 -0500

    lambda create room

---
+++ lambdas/room/createRoom.js
@@ -0,0 +1,26 @@
+import { langConfig, translations, httpCodes } from "../../commonIncludes";
+import { use, mongo, validateBody,authorizer } from "@octopy/serverless-core";
+import { roomSchema } from '../../schemas/room';
+import { createRoomDTO } from "../../models/room/createRoomDTO";
+
+
+const createRoom = async(event, context) => {
+    const { collections: [roomModel] } = event.useMongo;
+    const room = await roomModel.create(event.body);
+
+    return room
+}
+
+export const handler = use(createRoom, { httpCodes, langConfig, translations })
+    .use(authorizer({
+        uriDB: process.env.MONGO_CONNECTION, secretKey: process.env.SECRET_KEY,
+        roles: ["admin"]
+    }))
+    .use(validateBody(createRoomDTO, translations))
+    .use(mongo({ 
+        uri: process.env.MONGO_CONNECTION, 
+        models: ["rooms"], 
+        schemas: {
+            locations: roomSchema
+        } 
+    }));
