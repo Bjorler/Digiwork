718965d66e9738203f299ceb2bf7db6d474ba5e0
angelgbcore <barreraosorioangelgabriel@gmail.com>
2022-05-17 11:53:57 -0500

    [reservation] fix cancel

--- models/reservation/getReservationDTO.js
+++ models/reservation/getReservationDTO.js
@@ -2,7 +2,7 @@ import { Joi } from "@octopy/serverless-core";
 import { ReservationEnum } from "../../helpers/shared/enums";
 
 export const getReservationDTO = Joi.object({
-    type: Joi.string().valid(ReservationEnum.room, ReservationEnum.work_station).required().messages({
+    reservation_type: Joi.string().valid(ReservationEnum.room, ReservationEnum.work_station).required().messages({
         "string.base": "reservationTypeIsString",
         "any.only": "reservationTypeInvalid",
         "any.required": "reservationTypeRequired"
