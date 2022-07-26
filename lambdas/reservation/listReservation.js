de574e21347c050cb26ce71083643261841e0fd9
Bjorler <gth86@hotmail.com>
2022-07-26 18:23:12 -0500

    [reservation] email notification

--- lambdas/reservation/listReservation.js
+++ lambdas/reservation/listReservation.js
@@ -3,84 +3,31 @@ import { use, mongo, Model, authorizer, dayjs } from "@octopy/serverless-core";
 import { workStationReservationSchema, roomReservationSchema, parkingReservationSchema } from "../../schemas/reservation";
 import { locationSchema } from "../../schemas/location";
 import { ReservationEnum } from "../../helpers/shared/enums";
+import { parkingSchema } from "../../schemas/parking";
 
 const listReservation = async (event, context) => {
     const { collections: [wsReservationModel, roomReservationModel, parkingReservationModel] } = event.useMongo;
-    
-    // const location = (await Model(locationModel).getById(event.queryStringParameters.location))?.name || "";
-    const date = event.queryStringParameters?.date || "";
-    const type = event.queryStringParameters.type;
+   
+    const reservation_date = event.queryStringParameters?.date;
+    const reservation_type = event.queryStringParameters.type;
 
-    const collection = type === ReservationEnum.work_station 
-        ? wsReservationModel 
-        : ReservationEnum.room ? roomReservationModel
-        : parkingReservationModel;
-    const queryMatch = {
-        // "reservation.location.name": { $regex: location, $options: "i" },
-    };
-
-    if (date) {
-        queryMatch.date_formated = new Date(date).toISOString()
+    const filter = {};
+    if(reservation_date) {
+        filter.date_formated = new Date(date).toISOString();
     }
 
-    const reservations = await collection.aggregate([
-        {
-            $lookup: {
-                from: type === ReservationEnum.work_station ? "work_stations" 
-                : ReservationEnum.room ? "rooms" 
-                : "parkings",
-                localField: type === ReservationEnum.work_station ? "work_station" 
-                : ReservationEnum.room ? "room" 
-                : "parking",
-                pipeline: [
-                    {
-                        $lookup: {
-                            from: "locations",
-                            localField: "location",
-                            foreignField: "_id",
-                            as: "location",
-                        }
-                    },
-                    {
-                        $unwind: "$location"
-                    }
-                ],
-                foreignField: "_id",
-                as: "reservation",
-            },
-        },
-        // { $unwind: "$reservation" },
-        {
-            $lookup: {
-                from: "users",
-                localField: "user_id",
-                foreignField: "_id",
-                as: "user",
-            }
-        },     
-        // { $unwind: "$user" },
-        // {
-        //     $addFields: {
-        //         date_formated: { $dateToString: { format: "%Y-%m-%dT%H:00:00.000Z", date: "$start_date" } }
-        //     }
-        // },
-        // {
-        //     $match: queryMatch
-        // },
-        // {
-        //     $project: {
-        //         _id: 1,
-        //         user: "$user.name",
-        //         name: "$reservation.name",
-        //         start_date: 1,
-        //         end_date: 1,
-        //         reservation_type: type,
-        //         check_in: 1,
-        //         location: "$reservation.location.name",
-        //         public_id: 1
-        //     },
-        // }
-    ])
+    let reservations;
+    if(reservation_type == ReservationEnum.work_station) {
+        reservations = await wsReservationModel.find(filter).populate('user_id')
+            .populate({ path: 'work_stations', populate: { path: 'location' } })
+    } else if(reservation_type == ReservationEnum.room) {
+        reservations = await roomReservationModel.find(filter).populate('user_id')
+            .populate({ path: 'rooms', populate: { path: 'location' } })
+    } else {
+        reservations = await parkingReservationModel.find(filter).populate('user_id')
+            .populate({ path: 'parking', populate: { path: 'location' } })
+    }
+
 
     return reservations;
 }
@@ -92,11 +39,13 @@ export const handler = use(listReservation, { httpCodes, langConfig, translation
     }))
     .use(mongo({
         uri: process.env.MONGO_CONNECTION,
-        models: ["work_station_reservations", "room_reservations", "parking_reservations"],
+        models: ["work_station_reservations", "room_reservations", "parking_reservations",
+            "parkings", "location"],
         schemas: {
             work_station_reservations: workStationReservationSchema,
             room_reservations: roomReservationSchema,
             parking_reservations: parkingReservationSchema,
-            locations: locationSchema,
+            parkings: parkingSchema,
+            location: locationSchema,
         }
     }))
