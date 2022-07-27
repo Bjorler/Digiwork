0025162999c8fa069e94e3b31861a476af6205fd
Bjorler <gth86@hotmail.com>
2022-07-27 16:49:22 -0500

    [notification] deletemany

---
+++ lambdas/notification/deleteManyNotification.js
@@ -0,0 +1,25 @@
+import { langConfig, translations, httpCodes } from "../../commonIncludes";
+import { use, mongo, authorizer} from "@octopy/serverless-core";
+import { notificationSchema } from "../../schemas/notification";
+
+
+const deleteNotification = async(event, context) => {
+    const { collections: [notificationModel] } = event.useMongo;
+    const ids = event.queryStringParameters?.ids;
+    const notification = await notificationModel.deleteMany(ids);
+    
+    return notification;
+}
+
+export const handler = use(deleteNotification, { httpCodes, langConfig, translations })
+    .use(authorizer({
+        uriDB: process.env.MONGO_CONNECTION, secretKey: process.env.SECRET_KEY,
+        roles: ["admin", "user"]
+    })) 
+    .use(mongo({ 
+        uri: process.env.MONGO_CONNECTION, 
+        models: ["notifications"], 
+        schemas: {
+            notifications: notificationSchema
+        } 
+    }));
