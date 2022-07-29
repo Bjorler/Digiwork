72d2657b9f1b54b1f14810b6af84220c5f559a8e
SlideObject21 <gth86@hotmail.com>
2022-07-29 00:24:46 -0500

    [reservation] fix catalogue

--- lambdas/reservation/fullCatalogue.js
+++ lambdas/reservation/fullCatalogue.js
@@ -6,8 +6,8 @@ import { parkingSchema } from "../../schemas/parking";
 
 const fullCatalogue = async(event, context) => {
     const { collections: [roomModel, workStationModel, parkingModel] } = event.useMongo;
-    const type_filter = event.queryStringParameters.type;
-    
+    const type_filter = event.queryStringParameters?.type_filter;
+    console.log(type_filter);
     const query = [{
         $group: {
             _id: "$location",
@@ -19,14 +19,19 @@ const fullCatalogue = async(event, context) => {
             }
         }
     }];
-
+    
+    if(type_filter == 'room') {
+        return await roomModel.aggregate(query);
+    }
+   
+    if(type_filter == 'work_station') {
+        return await workStationModel.aggregate(query);
+    }
+    
     if (type_filter == 'parking') {
         return await parkingModel.aggregate(query);
     }
 
-    if(type_filter == 'room') {
-        return await roomModel.aggregate(query);
-    }
     
     return await workStationModel.aggregate(query);
 }
@@ -36,7 +41,7 @@ export const handler = use(fullCatalogue, { httpCodes, langConfig, translations 
         uriDB: process.env.MONGO_CONNECTION, secretKey: process.env.SECRET_KEY,
         roles: ["admin", "user"]
     }))
-    .use(mongo({ uri: process.env.MONGO_CONNECTION, models: ["rooms", "work_stations"], schemas: {
+    .use(mongo({ uri: process.env.MONGO_CONNECTION, models: ["rooms", "work_stations", "parkings"], schemas: {
         room: roomSchema,
         work_stations: workStationSchema,
         parking: parkingSchema
